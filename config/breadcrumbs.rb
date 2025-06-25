aa9bfccfb3895da5a196d980bbd17e7520157df9
mizuki-mimc <mizuki.o.110529@gmail.com>
2025-06-25 22:16:20 +0900

    add:ストーリー編集ページの追加

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -6,3 +6,8 @@ crumb :story do |story|
   link truncate(story.title, length: 10), story_path(story)
   parent :stories
 end
+
+crumb :edit_story do |story|
+  link "ストーリー編集", edit_story_path(story)
+  parent :story, story
+end
