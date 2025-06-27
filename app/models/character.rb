d6766afee7f7c784c5927e0c2187f6325f6c1086
mizuki-mimc <mizuki.o.110529@gmail.com>
2025-06-27 17:16:30 +0900

    fix:Lintチェック修正

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,7 +3,7 @@ class Character < ApplicationRecord
 
   belongs_to :birthplace_world_guide, class_name: "WorldGuide", optional: true
   belongs_to :address_world_guide, class_name: "WorldGuide", optional: true
-  
+
   enum category: {
     present: "現在",
     future: "未来",
