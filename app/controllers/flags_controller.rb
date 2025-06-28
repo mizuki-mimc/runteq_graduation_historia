6d31aec92343bb013a56d5641c7c57f13387f114
mizuki-mimc <mizuki.o.110529@gmail.com>
2025-06-28 18:54:32 +0900

    add:フラグ作成、詳細ページの実装

--- app/controllers/flags_controller.rb
+++ app/controllers/flags_controller.rb
@@ -1,14 +1,57 @@
 class FlagsController < ApplicationController
   before_action :require_login
   before_action :set_story
+  before_action :set_flag, only: [ :show, :edit, :update, :destroy ]
 
   def index
-    @flags = @story.flags.order(:created_at)
+    @flags = @story.flags.includes(:plot_flags).order(:created_at)
+  end
+
+  def show
+  end
+
+  def new
+    @flag = @story.flags.build
+  end
+
+  def create
+    @flag = @story.flags.build(flag_params)
+    if @flag.save
+      redirect_to story_flags_path(@story), success: "フラグを作成しました。"
+    else
+      flash.now[:error] = "フラグの作成に失敗しました。"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @flag.destroy
+    redirect_to story_flags_path(@story), success: "フラグを削除しました。", status: :see_other
+  end
+
+  def update
+    if @flag.update(flag_params)
+      redirect_to story_flag_path(@story, @flag), success: "フラグを更新しました。"
+    else
+      flash.now[:error] = "フラグの更新に失敗しました。"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
   end
 
   private
 
   def set_story
     @story = current_user.stories.find(params[:story_id])
+  end
+
+  def set_flag
+    @flag = @story.flags.includes(plot_flags: :plot).find(params[:id])
+  end
+
+  def flag_params
+    params.require(:flag).permit(:title, :content, :check_created, :check_recovered)
   end
 end
--- app/helpers/flags_helper.rb
+++ app/helpers/flags_helper.rb
@@ -1,2 +1,18 @@
 module FlagsHelper
+  def flag_status_indicator(flag)
+    image_content = ""
+    box_classes = "h-8 w-8 border-2 rounded-md flex items-center justify-center"
+
+    if flag.recovered?
+      box_classes += " border-green-500"
+      image_content = image_tag("flag_recovered.png", class: "w-5 h-5")
+    elsif flag.created?
+      box_classes += " border-blue-500"
+      image_content = image_tag("flag_placed.png", class: "w-5 h-5")
+    else
+      box_classes += " border-gray-300"
+    end
+
+    content_tag(:div, image_content, class: box_classes)
+  end
 end
