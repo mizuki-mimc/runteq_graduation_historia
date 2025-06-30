5b92513c3405e6a39c70f717eb296aeec4036f95
mizuki-mimc <mizuki.o.110529@gmail.com>
2025-06-30 21:42:23 +0900

    add-管理者ツール統計

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < Admin::BaseController
   def index
-    @users = User.all
+    @user_count = User.count
+    @story_count = Story.count
+    @users_this_week = User.where("created_at >= ?", 1.week.ago).count
+    @stories_this_week = Story.where("created_at >= ?", 1.week.ago).count
+    @recent_users = User.where("created_at >= ?", 1.week.ago).order(created_at: :desc)
   end
 end
