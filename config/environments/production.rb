7ef3bcf449e99f7e52e754a6cbe6ebc773ba53ef
mizuki-mimc <mizuki.o.110529@gmail.com>
2025-07-06 13:10:24 +0900

    fix-render_error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,6 +102,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.hosts << "historia.onrender.com"
-  config.hosts << "historia-staging.onrender.com"
+  config.hosts << "historia-web-service.onrender.com"
+  config.hosts << "historia-staging.onrender.com" 
 end
