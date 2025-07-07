cb0f1abe93d46fee507cc126a7d0d1b6872c71b5
mizuki-mimc <mizuki.o.110529@gmail.com>
2025-07-07 12:00:07 +0900

    fix-Googleログインの脆弱性修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-OmniAuth.config.allowed_request_methods = [ :get, :post ]
+OmniAuth.config.allowed_request_methods = [ :post ]
 
 OmniAuth.config.on_failure = Proc.new { |env|
   OmniAuth::FailureEndpoint.new(env).redirect_to_failure
